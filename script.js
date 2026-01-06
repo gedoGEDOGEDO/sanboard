6141d3ec9c42a7b4d2e95990c08ae7e941e44e00
gedoGEDOGEDO <kehhy1999@gmail.com>
2026-01-06 13:43:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-fetch("data.json")
-  .then(res => res.json())
+fetch("./data.json")
+  .then(res => {
+    if (!res.ok) throw new Error("Не удалось загрузить data.json");
+    return res.json();
+  })
   .then(data => {
     const list = document.getElementById("chart");
     const type = list.dataset.type;
@@ -10,24 +13,22 @@ fetch("data.json")
     items.forEach((item, index) => {
       const pos = index + 1;
 
+      const cover = item.cover ? item.cover : "covers/default.jpg";
+
       const card = document.createElement("div");
       card.className = "chart-card";
 
       card.innerHTML = `
         <div class="chart-rank">${pos}</div>
-
         <div class="chart-cover">
-          <img src="${item.cover}" alt="${item.title}">
+          <img src="${cover}" alt="${item.title}">
         </div>
-
-
         <div class="chart-info">
           <div class="chart-title">${item.title}</div>
           <div class="chart-artist">${item.artist}</div>
         </div>
-
         <div class="chart-stats">
-          <div><b>${item.weeks}</b><span>недель</span></div>
+          <div><b>${item.weeks}</b><span>недель в чарте</span></div>
           <div><b>${item.peak}</b><span>пик</span></div>
           <div><b>${item.weeksAt1}</b><span>нед. #1</span></div>
         </div>
@@ -35,9 +36,5 @@ fetch("data.json")
 
       list.appendChild(card);
     });
-  });
-
-
-
-
-
+  })
+  .catch(err => console.error(err));
